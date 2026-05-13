fe9fa95abf70e0707283b525d250cc6247292a9b
Dominic <dominic.cisneros@udla.edu.ec>
2026-05-13 14:58:50 -0500

    tercer y ultimo commit del segundo ejercicio

--- 2do_ejercicio.c
+++ 2do_ejercicio.c
@@ -4,8 +4,11 @@ int main() {
     char vocales[5] = {'a', 'e', 'i', 'o', 'u'};
     char *ptr;
     ptr = &vocales[0];
+
+    *(ptr+4) = 'x';
+
     for (int i = 0; i < 5; i++) {
-        printf("vocales[%d] = %c\n", i, *(ptr + i));
+        printf("vocales[%d] = %c %c\n", i, *(ptr + i), *(ptr + i));
     }
 
     return 0;
