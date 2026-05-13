a29a4117de29a71d58ff208df255b9578202f78c
Dominic <dominic.cisneros@udla.edu.ec>
2026-05-13 14:23:24 -0500

    Segundo versionamiento, con puntero

--- 2pm.c
+++ 2pm.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    int numeros[5] = {2,4,6,8,10};
+int main (int argc, char *argv[]) {
+    int numeros[] = {2, 4, 6, 8, 10};
+    int *ptr;
+    ptr = &numeros[0];
+    //printf("%d\n", *ptr);
 
-    for(int i = 0; i < 5; i++){
-        printf("Elemento %d: %d\n", i, numeros[i]);
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", numeros[i]);
+        printf("%d\n", *(ptr + i));
     }
-
     return 0;
 }
